589294df522e6bfde9a35ea6a58c2743d8bad4d4
zsombor-flds <zsombor.foldesi@hiflylabs.com>
2025-04-18 16:56:50 +0200

    run.dev change config dirs to read-only

--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -7,27 +7,29 @@ HISTFILE="$HOME/denv/.docker_zsh_history"
 WORKSPACE="$HOME/workspace"
 GITCONFIG="$HOME/.gitconfig"
 SSH_DIR="$HOME/.ssh"
+IMAGE_NAME="ghcr.io/zsombor-flds/denv"
 
 # Create parent dir for history file
 mkdir -p "$(dirname "$HISTFILE")"
 touch "$HISTFILE"
 
+
 echo "Docker development environment started with:"
 echo "Using workspace: $WORKSPACE"
 echo "Using history file: $HISTFILE"
 echo "Using gitconfig: $GITCONFIG"
-echo "Using SSH dir: $SSH_DIR"
+echo "Using SSH dir (read-only): $SSH_DIR"
 
 # Run Docker container
 docker run -it \
   --rm \
   --name denv \
+  --network=host \
+  --privileged \
   -v "$WORKSPACE":/workspace \
   -v "$HISTFILE":/root/.zsh/history \
   -v "$GITCONFIG":/root/.gitconfig:ro \
   -v "$SSH_DIR":/root/.ssh:ro \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  --network=host \
-  --privileged \
-  ghcr.io/zsombor-flds/denv \
+  "$IMAGE_NAME" \
   zsh
